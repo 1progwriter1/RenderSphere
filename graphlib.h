7b9b6b202e2b1078167f0bcc432a38da0774eaf5
1progwriter1 <112411575+1progwriter1@users.noreply.github.com>
2024-09-09 23:40:37 +0300

    resolve conflicts

---
+++ graphlib.h
@@ -0,0 +1,24 @@
+#ifndef WINDOW_FUNCTIONS
+#define WINDOW_FUNCTIONS
+
+#include <SFML/Graphics.hpp>
+#include <cstddef>
+#include <vector>
+
+struct WindowData : public sf::Window
+{
+    sf::RenderWindow window;
+    sf::VertexArray pixels;
+    unsigned int height;
+    unsigned int width;
+
+    sf::Vertex *lines;
+    size_t num_of_lines;
+
+    WindowData( unsigned int init_width, unsigned init_height, const char *label = "hello");
+    ~WindowData();
+};
+
+void drawLines( WindowData *data);
+
+#endif // WINDOW_FUNCTIONS
